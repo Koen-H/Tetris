24b5d1b93db5bd23f3d8da617793ab25a9bf8b67
KoendertNL <49477924+KoendertNL@users.noreply.github.com>
2022-01-27 16:58:26 +0100

    gitignore

---
+++ GXPEngine/bin/Debug/tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tilesheet" tilewidth="25" tileheight="25" tilecount="2" columns="2">
+ <image source="tilesheet.png" width="50" height="25"/>
+</tileset>
